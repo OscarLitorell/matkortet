182614b8238365b70dfee4496dc06f2e31ca4a28
Oscar Litorell <oscar.litorell@gmail.com>
2019-11-17 00:50:48 +0100

    Update views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+    res.render('index')
+})
 
-module.exports = router;
+router.get("/map", (req, res, next) => {
+    res.render("map")
+})
+
+
+
+module.exports = router
