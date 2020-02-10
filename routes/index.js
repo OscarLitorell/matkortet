fca9dbc9f8cac4e7ce86a745f3760fee3da8a2db
Oscar Litorell <oscar.litorell@gmail.com>
2020-02-10 17:33:56 +0100

    Created time-left page

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,15 @@ router.get('/', function(req, res, next) {
     res.render('index')
 })
 
+/* GET map page */
 router.get("/map", (req, res, next) => {
     res.render("map")
 })
 
+/* GET time-left page */
+router.get("/time-left", (req, res, next) => {
+    res.render("time-left")
+})
 
 
 module.exports = router
