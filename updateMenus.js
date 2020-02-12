a70d595e21e428382aee03363f319de87e280cde
Oscar Litorell <oscar.litorell@gmail.com>
2020-02-12 23:27:45 +0100

    Update updateMenus.js
    Now uses the new node scripts instead of python.

--- updateMenus.js
+++ updateMenus.js
@@ -1,9 +1,26 @@
-const { spawn } = require("child_process")
+
+const fs = require("fs")
+const glob = require("glob")
+
 
 function update() {
-    spawn("python", ["update_menus.py"])
+    glob("restaurants/*", (err, dirs) => {
+
+        let menus = []
+
+        dirs.forEach(dir => {
+            let menuGetter = require("./" + dir + "/getMenu.js")
+            menus.push(menuGetter())
+        })
+
+        Promise.all(menus).then(values => {
+            fs.writeFile("./public/menus.json", JSON.stringify(values), () => {})
+        })
+    })
 }
+
 
 update()
 
 setInterval(update, 3600000)
+
